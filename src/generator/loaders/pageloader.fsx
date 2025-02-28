a16058c6cace500b648bf8a60cdf96006bd3bab5
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2025-02-28 06:33:57 -0500

    fixed pageloader recursion bug and added pages

--- src/generator/loaders/pageloader.fsx
+++ src/generator/loaders/pageloader.fsx
@@ -1,13 +1,43 @@
 #r "../_lib/Fornax.Core.dll"
 
+open System.IO
+open System
+
 type Page = {
     title: string
     link: string
+    content: string
 }
 
+let isValidPage (filePath: string) =
+    let ext = Path.GetExtension(filePath)
+    let dir = Path.GetDirectoryName(filePath)
+    ext = ".md" && 
+    not (dir.Contains("_public")) && 
+    not (Path.GetFileName(filePath).StartsWith("_"))
+
+let titleFromPath (filePath: string) =
+    let fileName = Path.GetFileNameWithoutExtension(filePath)
+    if fileName = "index" then "Home"
+    else fileName |> fun s -> Char.ToUpper(s.[0]).ToString() + s.Substring(1)
+
 let loader (projectRoot: string) (siteContent: SiteContents) =
-    siteContent.Add({title = "Home"; link = "/"})
-    siteContent.Add({title = "About"; link = "/about.html"})
-    siteContent.Add({title = "Contact"; link = "/contact.html"})
+    let pagesPath = Path.Combine(projectRoot, "pages")
+    
+    Directory.GetFiles(pagesPath, "*.md", SearchOption.AllDirectories)
+    |> Array.filter isValidPage
+    |> Array.map (fun filePath ->
+        let link = 
+            filePath.Substring(pagesPath.Length)
+                .Replace("\\", "/")
+                .Replace(".md", ".html")
+                .Replace("index.html", "/")
+        let content = File.ReadAllText filePath
+        {
+            title = titleFromPath filePath
+            link = link
+            content = content
+        })
+    |> Array.iter siteContent.Add
 
     siteContent
---
+++ src/generator/pages/index.md
@@ -0,0 +1,9 @@
+# Welcome to FornaxHome
+
+This is the home page of our site. Built with Fornax and Suave.
+
+## Features
+
+- Static site generation with F#
+- Modern responsive design
+- Fast and lightweight
